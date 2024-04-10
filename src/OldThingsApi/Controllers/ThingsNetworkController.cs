05d7db3cab0de6ac4a2b3b0aea424aca4186e3e2
SeBastArt <sebastian.schueler1@gmx.de>
2024-04-10 18:34:53 +0200

    SDK 6.0

---
+++ src/OldThingsApi/Controllers/ThingsNetworkController.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OldThingsApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ThingsNetworkController : ControllerBase
+{
+    private readonly IInfluxDbService _service;
+    
+    public ThingsNetworkController(IInfluxDbService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost("PostDataPoint", Name = "PostDataPoint")]
+    public ActionResult PostDataPoint([FromBody] JsonElement data)
+    {
+        _service.PostDataPoint(data);
+        return Ok();
+    }
+}
