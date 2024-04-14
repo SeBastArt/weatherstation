fdc2ab8d232bb12aeeda2aff87db09ab922d1af8
SeBastArt <sebastian.schueler1@gmx.de>
2024-04-14 21:57:26 +0200

    backend to frontend

--- src/Weatherstation/Pages/Index.cshtml.cs
+++ src/Weatherstation/Pages/Index.cshtml.cs
@@ -6,13 +6,22 @@ namespace Weatherstation.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IConfiguration _configuration;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public void OnGet()
     {
+        // httpClient to https://backend/api/Temperature
+        var backendApi = new Uri(_configuration["BackendApi:Url"] ?? throw new InvalidOperationException());
+        var httpClient = new HttpClient();
+        httpClient.BaseAddress = backendApi;
+        var response = httpClient.GetAsync("/api/ThingsNetwork/GetTemperature?eui=dfgdfg'").Result;
+        var content = response.Content.ReadAsStringAsync().Result;
+        ViewData["Temperature"] = content;
     }
 }
